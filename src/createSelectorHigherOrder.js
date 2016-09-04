e59847d5f9fe17438c54d46f2095177dd26761d6
Brian Neisler <brian@airbug.com>
2016-09-04 00:43:16 -0700

    Few bug fixes. Bump version to 0.0.2

--- src/createSelectorHigherOrder.js
+++ src/createSelectorHigherOrder.js
@@ -1,8 +1,8 @@
 import _ from 'mudash'
 import o from 'duxtape'
 
-export default function createSelectorHigherOrder(selectors) {
-  const selector = o.reduceReducers(selectors)
+export default function createSelectorHigherOrder(selectors = []) {
+  const selector = o.reduceReducers(...selectors)
   return reducer => (state, action) => {
     let prevState = state
     state = reducer(state, action)
